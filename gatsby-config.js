da107350407e5c5578058037e97e747a114ca09b
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2020-04-06 23:52:41 +0800

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,13 +61,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Weekly code snippets`,
+        short_name: `Weekly code snippets`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ff0083`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/coding-bits.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
